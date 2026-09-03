3d3739cdcc0147bb89553d798f7aa54ac4714944
TiaraBasori <mintabyss233@gmail.com>
2026-09-04 02:29:04 +0800

    fix: telegram_mini_app_view — _openPopup 中 ?id 语法错误导致无法弹出对话框

--- lib/chat/telegram_mini_app_view.dart
+++ lib/chat/telegram_mini_app_view.dart
@@ -916,7 +916,7 @@ class _TelegramMiniAppViewState extends State<TelegramMiniAppView>
       ],
     );
     _popupOpen = false;
-    await _emitEvent('popup_closed', {'button_id': ?id});
+    await _emitEvent('popup_closed', {'button_id': id});
   }
 
   Future<String?> _showNativeDialog({
