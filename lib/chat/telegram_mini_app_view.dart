2de2c213266d0422e1bedda4861fc20769e85c3e
TiaraBasori <mintabyss233@gmail.com>
2026-09-05 21:38:00 +0800

    wip: pr82

--- lib/chat/telegram_mini_app_view.dart
+++ lib/chat/telegram_mini_app_view.dart
@@ -1230,7 +1230,9 @@ class _TelegramMiniAppViewState extends State<TelegramMiniAppView>
       ],
     );
     _popupOpen = false;
-    await _emitEvent('popup_closed', {'button_id': ?id});
+    if (id != null) {
+      await _emitEvent('popup_closed', {'button_id': id});
+    }
   }
 
   Future<String?> _showNativeDialog({
